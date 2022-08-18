53e71a45669f3ff7f7f1e0ca83ea2d29aa6b0311
Дарья <yu.dar@yandex.ru>
2022-08-19 00:45:21 +0300

    Главный экран

--- source/js/main.js
+++ source/js/main.js
@@ -20,6 +20,21 @@ window.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
+
 // ---------------------------------
 
 // ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
